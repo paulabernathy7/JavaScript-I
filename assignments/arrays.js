eb2c6afa0c1f3d44dae6f4ac69e125f0326cf51b
Paul Abernathy <paul.abernathy7@gmail.com>
2019-03-28 19:31:50 -0400

    3rd commit

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -72,7 +72,12 @@ console.log(lastCar.car_make, lastCar.car_model);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-console.log(inventory.car_model.sort());
+let myCarModels = [];
+for (let i = 0; i < inventory.length; i ++) {
+    console.log(inventory[i].car_model)
+    myCarModels.push(inventory[i].car_model);
+}
+console.log(myCarModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
