1c9a7fdaa253a6e8cc0c3e1abb45bf28ab8677b4
Paul Abernathy <paul.abernathy7@gmail.com>
2019-03-29 17:23:51 -0400

    fixed last array

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -103,7 +103,7 @@ console.log(oldCars.length);
 let BMWAndAudi = [];
 
 for (let i = 0; i < inventory.length; i ++) {
-    if (inventory[i].car_make === "BMW" || "Audi"){
+    if (inventory[i].car_make === "BMW" || inventory[i].car_make === "Audi"){
         BMWAndAudi.push(inventory[i])
     }
 }
